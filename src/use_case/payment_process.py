fd9fff29d19f42d42c28564a8490f7cc13441c61
Johnathan Barbosa <jbarbosa0504@gmail.com>
2026-04-27 13:34:38 -0300

    refactor: create message function

--- src/use_case/payment_process.py
+++ src/use_case/payment_process.py
@@ -18,8 +18,12 @@ def process_credit_card_payment(
     purchaser_document: str,
     purchase_value: Decimal,
     store_document: str,
-) -> str:
+) -> bool:
     # Essa função faz o processamento do pagamento, utilizando cartão de credito
+    return True
+
+
+def credit_card_message(store_document: str) -> str:
     return f"A Cielo processou o pagamento para a loja {store_document}"
 
 
@@ -27,25 +31,35 @@ def process_debit_card_payment(
     purchaser_document: str,
     purchase_value: Decimal,
     store_document: str,
-) -> str:
+) -> bool:
     # Essa função faz o processamento do pagamento, utilizando cartão de débito
+    return True
+
+
+def debit_card_message(purchaser_document: str) -> str:
     return f"A Cielo processou uma compra no cartão de débito do cliente {purchaser_document}"
 
 
 class PaymentProcessUseCase:
     def execute(self, payment_process_dto: PaymentProcessDTO) -> str:
         if payment_process_dto.payment_method == "credit_card":
-            return process_credit_card_payment(
+            process_credit_card_payment(
                 purchaser_document=payment_process_dto.purchaser_document,
                 purchase_value=payment_process_dto.purchase_value,
                 store_document=payment_process_dto.store_document,
             )
+            return credit_card_message(
+                store_document=payment_process_dto.store_document,
+            )
 
         if payment_process_dto.payment_method == "debit_card":
-            return process_debit_card_payment(
+            process_debit_card_payment(
                 purchaser_document=payment_process_dto.purchaser_document,
                 purchase_value=payment_process_dto.purchase_value,
                 store_document=payment_process_dto.store_document,
+            )
+            return debit_card_message(
+                purchaser_document=payment_process_dto.purchaser_document,
             )
 
         raise PaymentProcessError("Método de pagamento não suportado")
